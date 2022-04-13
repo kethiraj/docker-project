3d89393a6c7d6da89abdcb5c525cee4ca505a6d2
kethiraj <54640211+kethiraj@users.noreply.github.com>
2022-04-13 15:21:46 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -3,6 +3,5 @@
 # Specify the provider and access details
 provider "aws" {
 #  shared_credentials_file = "C:\Users\ethi9\.aws\edentials"
-  profile                 = "default"
   region                  = "ap-south-1"
 }
