646b6ffd44169885adc5157188eb1fa654ed7985
kethiraj <54640211+kethiraj@users.noreply.github.com>
2022-04-24 22:08:28 +0530

    Update autoscaling.tf

--- terraform/autoscaling.tf
+++ terraform/autoscaling.tf
@@ -16,7 +16,7 @@ resource "aws_autoscaling_group" "failure_analysis_ecs_asg" {
   name                 = "asg"
   vpc_zone_identifier  = [aws_subnet.public[0].id]
   launch_configuration = aws_launch_configuration.ecs_launch_config.name
-  target_group_arns    = aws_alb_target_group.app.arn
+  target_group_arns    = [aws_alb_target_group.app.arn]
 
   desired_capacity          = 1
   min_size                  = 1
